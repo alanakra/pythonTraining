6e0f773ec647e7acaf9cfc5c4d62a792e51b8a62
Alan Akra <alanakra@hotmail.fr>
2024-05-27 09:12:03 +0000

    Add multSum

---
+++ multsum.py
@@ -0,0 +1,12 @@
+#https://pynative.com/python-basic-exercise-for-beginners/
+
+
+def multSum(num1, num2):
+ prod = num1 * num2
+ sum = num1 + num2
+ if (num1 + num2 <= 1000): 
+  return sum
+ else:
+  return prod
+
+print('The result is ' + str(multSum(399, 294)))
